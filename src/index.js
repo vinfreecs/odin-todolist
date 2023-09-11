edffb59e64b83944dbef47ef23b65119f9280704
vinfreecs <89305950+vinfreecs@users.noreply.github.com~>
2023-09-11 22:00:27 +0530

    few more funtionalites done

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import "./styles/style.css";
 function Collection() {
     let collection = [];
     let beginProj = new Project("Getting Started ...")
-    let beginNote = new Note("New Note")
+    let beginNote = new Note("New Note","Add any info you wamt hear....","11-09-2023")
     beginProj.notes.push(beginNote)
     collection.push(beginProj)
     let currentProj = collection[0];
-    console.log(currentProj);
     this.getCurrentProject = () => currentProj;
     this.updateCurrentProject = function (proj) {
         currentProj = proj
@@ -28,15 +27,16 @@ function Collection() {
         }
 
     }
-    this.addNoteToProject = function (proj, title) {
-        let note = new Note(title)
+    this.addNoteToProject = function (proj, title,description,dueDate) {
+        let note = new Note(title,description,dueDate)
         proj.notes.push(note);
     };
     this.deleteProject = function (proj) {
         collection = collection.filter((item) => item !== proj);
     };
-    this.deleteNote = function (proj, note) {
-        proj.notes = proj.notes.filter((item) => item !== note);
+    this.deleteNote = function (noteId) {
+        currentProj.notes = currentProj.notes.filter((item) => item.id !== noteId);
+        
     };
     this.getProjectByID = function (name) {
         for (let i = 0; i < collection.length; i++) {
@@ -50,11 +50,11 @@ function Project(name) {
     this.name = name;
     this.notes = [];
 }
-function Note(title, description, dueDate, priority) {
+function Note(title, description, dueDate) {
+    this.id = Date.now();
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
-    this.priority = priority;
     //   this.check = false;
     //   this.handleCheck = function (proj, note) {
     //     this.check = true;
@@ -75,24 +75,8 @@ function ScreenController() {
             projectsDiv.appendChild(projectDiv)
         })
     }
-    //adding projects to collections
-    let projectForm = document.querySelector(".project-form")
-    projectForm.addEventListener("submit", (e) => {
-        e.preventDefault()
-        console.log(main.getCurrentProject())
-        let name = document.getElementById("project-name")
-        if (name.value === "") {
-            alert("Enter some text!")
-        } else {
-            main.addProject(name.value)
-        }
-        name.value = ""
-        updateScreen();
-    })
-    console.log(main.getCurrentProject())
     function updateNotes() {
         let proj = main.getCurrentProject()
-        console.log(main.getCurrentProject())
         const rightDiv = document.querySelector(".right")
         const notesDiv = document.querySelector(".notes")
         rightDiv.style.display = "block"
@@ -105,7 +89,20 @@ function ScreenController() {
                     const title = document.createElement("h3")
                     title.classList = "note-title"
                     title.textContent = ele.title
+                    const description = document.createElement("p")
+                    description.classList = "note-description"
+                    description.textContent = ele.description
+                    const dueDate = document.createElement("p")
+                    dueDate.classList = "note-due-date"
+                    dueDate.textContent = ele.dueDate
+                    const deleteNote = document.createElement("button")
+                    deleteNote.classList = "note-delete"
+                    deleteNote.textContent = "del"
+                    deleteNote.id = ele.id
                     note.append(title)
+                    note.append(description)
+                    note.append(dueDate)
+                    note.append(deleteNote)
                     notesDiv.append(note)
                 })
             }
@@ -119,26 +116,56 @@ function ScreenController() {
         })
         updateNotes()
     }
-    projectsDiv.addEventListener("click", upCurrentProj)
-    let noteForm = document.querySelector(".note-form")
-    noteForm.addEventListener("submit", (event) => {
-        event.preventDefault()
-        console.log(main.getCurrentProject())
+    function addNotes(e){
+        e.preventDefault()
         let noteTitle = document.getElementById("note-title")
+        let noteDescription = document.getElementById("note-description")
+        let noteDueDate = document.getElementById("note-due-date")
         if (noteTitle.value === "") {
             alert("Enter some text")
         } else {
             projectsList.forEach(item => {
                 if (item.name === main.getCurrentProject().name) {
-                    main.addNoteToProject(item, noteTitle.value)
+                    main.addNoteToProject(item, noteTitle.value,noteDescription.value,noteDueDate.value)
                 }
             });
-            console.log(noteTitle.value);
             updateNotes()
         }
-    })
+        noteTitle.value = ""
+        noteDescription.value = ""
+        noteDueDate.value= ""
+    }
+    function addProjects(e){
+        e.preventDefault()
+        let name = document.getElementById("project-name")
+        if (name.value === "") {
+            alert("Enter some text!")
+        } else {
+            main.addProject(name.value)
+        }
+        name.value = ""
+        updateScreen();
+    }
+    function deleteNote(e){
+        if(e.target.classList.value === "note-delete"){
+            main.deleteNote(parseInt(e.target.id))
+            updateNotes();
+        }
+    }
+    //adding projects to collections
+    let projectForm = document.querySelector(".project-form")
+    projectForm.addEventListener("submit", addProjects)
+    //updating currentProject selected
+    projectsDiv.addEventListener("click", upCurrentProj)
+    //taking input from form and updating notes to respective project
+    let noteForm = document.querySelector(".note-form")
+    noteForm.addEventListener("submit",addNotes)
+    //deleting notes
+    document.addEventListener("click",deleteNote)
+    
     console.log(main.getCollection());
     updateScreen();
+    updateNotes();
 }
 ScreenController();
 // console.log(proj1.name)
